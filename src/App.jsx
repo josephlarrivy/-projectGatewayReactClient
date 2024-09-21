1d1be567aae2c1e101a3c26001156d0c98dc1a45
josephlarrivy <joseph.larrivy@gmail.com>
2024-09-21 18:28:29 -0500

    added a test gateway request

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const doTestRequest = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/data/test/testGateway')
+      console.log(response)
+    } catch (error) {
+      console.error("Error making the request", error)
+    }
+  }
+
+  useEffect(() => {
+    doTestRequest()
+  }, [])
 
   return (
     <div>
